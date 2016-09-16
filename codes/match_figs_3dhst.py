e1fc199d23870ad5506f0706079affc0c6a8e595
Bhavin Joshi <bajoshi@asu.edu>
2016-09-15 21:56:50 -0700

    Initial commit

---
+++ codes/match_figs_3dhst.py
@@ -0,0 +1,94 @@
+from __future__ import division
+
+import numpy as np
+from astropy.io import fits
+
+import os
+import sys
+
+home = os.getenv('HOME')  # Does not have a trailing slash at the end
+massive_galaxies_dir = home + "/Desktop/FIGS/massive-galaxies/"
+
+sys.path.append(home)
+import matching as mt
+
+if __name__ == '__main__':
+    
+    # Read 3D-HST catalog
+    threed_hst_cat = fits.open('/Users/bhavinjoshi/Documents/3D-HST/3dhst.v4.1.5.master.fits')
+
+    # Make intermediate catalogs for 3DHST
+    threed_goodsn = np.where(threed_hst_cat[1].data['field'] == 'goodsn')[0]
+    threed_goodss = np.where(threed_hst_cat[1].data['field'] == 'goodss')[0]
+
+    # Read in FIGS catalogs # latest version v1.2
+    gn1cat = np.genfromtxt(massive_galaxies_dir + 'GN1_prelim_science_v1.2.cat', dtype=None,\
+                           names=['id','ra','dec'], usecols=([2,3,4]), skip_header=25)
+    gn2cat = np.genfromtxt(massive_galaxies_dir + 'GN2_prelim_science_v1.2.cat', dtype=None,\
+                           names=['id','ra','dec'], usecols=([2,3,4]), skip_header=25)
+    gs1cat = np.genfromtxt(massive_galaxies_dir + 'GS1_prelim_science_v1.2.cat', dtype=None,\
+                           names=['id','ra','dec'], usecols=([2,3,4]), skip_header=25)
+    gs2cat = np.genfromtxt(massive_galaxies_dir + 'GS2_prelim_science_v1.2.cat', dtype=None,\
+                           names=['id','ra','dec'], usecols=([2,3,4]), skip_header=25)
+
+    # Number of objects in each FIGS field
+    # GN1: 7271
+    # GN2: 3810
+    # GS1: 8193
+    # GS2: 6354
+    # Total: 25628
+
+    # Create arrays for ra and dec for both catalogs
+    threed_n_ra = threed_hst_cat[1].data['ra'][threed_goodsn]
+    threed_n_dec = threed_hst_cat[1].data['dec'][threed_goodsn]
+    threed_s_ra = threed_hst_cat[1].data['ra'][threed_goodss]
+    threed_s_dec = threed_hst_cat[1].data['dec'][threed_goodss]
+
+    gn1_ra = gn1cat['ra']
+    gn1_dec = gn1cat['dec']
+
+    gn2_ra = gn2cat['ra']
+    gn2_dec = gn2cat['dec']
+
+    gs1_ra = gs1cat['ra']
+    gs1_dec = gs1cat['dec']
+
+    gs2_ra = gs2cat['ra']
+    gs2_dec = gs2cat['dec']
+
+    # Run the matching function
+    # GN1
+    deltaRA, deltaDEC, threed_gn1_ra_matches, threed_gn1_dec_matches, figs_gn1_ra_matches, figs_gn1_dec_matches, num_single_matches = \
+    mt.match(threed_n_ra, threed_n_dec, gn1_ra, gn1_dec, lim=0.3*1/3600)
+
+    print "Single matches GN1 - ", num_single_matches, "out of", len(gn1cat)
+    mt.plot_diff(deltaRA, deltaDEC, name='gn1')
+
+    # GN2
+    deltaRA, deltaDEC, threed_gn2_ra_matches, threed_gn2_dec_matches, figs_gn2_ra_matches, figs_gn2_dec_matches, num_single_matches = \
+    mt.match(threed_n_ra, threed_n_dec, gn2_ra, gn2_dec, lim=0.3*1/3600)
+
+    print "Single matches GN2 - ", num_single_matches, "out of", len(gn2cat)
+    mt.plot_diff(deltaRA, deltaDEC, name='gn2')
+
+    # GS1
+    deltaRA, deltaDEC, threed_gs1_ra_matches, threed_gs1_dec_matches, figs_gs1_ra_matches, figs_gs1_dec_matches, num_single_matches = \
+    mt.match(threed_s_ra, threed_s_dec, gs1_ra, gs1_dec, lim=0.3*1/3600)
+
+    print "Single matches GS1 - ", num_single_matches, "out of", len(gs1cat)
+    mt.plot_diff(deltaRA, deltaDEC, name='gs1')
+
+    # GS2
+    deltaRA, deltaDEC, threed_gs2_ra_matches, threed_gs2_dec_matches, figs_gs2_ra_matches, figs_gs2_dec_matches, num_single_matches = \
+    mt.match(threed_s_ra, threed_s_dec, gs2_ra, gs2_dec, lim=0.3*1/3600)
+
+    print "Single matches GS2 - ", num_single_matches, "out of", len(gs2cat)
+    raoff, decoff = mt.findoffset(deltaRA, deltaDEC)
+    gs2_ra += raoff
+    deltaRA, deltaDEC, threed_gs2_ra_matches, threed_gs2_dec_matches, figs_gs2_ra_matches, figs_gs2_dec_matches, num_single_matches = \
+    mt.match(threed_s_ra, threed_s_dec, gs2_ra, gs2_dec, lim=0.3*1/3600)
+    print "Single matches GS2 - ", num_single_matches, "out of", len(gs2cat)
+    raoff, decoff = mt.findoffset(deltaRA, deltaDEC)
+    mt.plot_diff(deltaRA, deltaDEC, name='gs2')
+
+    sys.exit(0)
---
+++ codes/plotspectra.py
@@ -0,0 +1,71 @@
+from __future__ import division
+
+import numpy as np
+from astropy.io import fits
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+home = os.getenv('HOME')  # Does not have a trailing slash at the end
+massive_galaxies_dir = home + "/Desktop/FIGS/massive-galaxies/"
+massive_figures_dir = massive_galaxies_dir + "figures/"
+savefits_dir = home + "/Desktop/FIGS/new_codes/"
+stacking_analysis_dir = home + "/Desktop/FIGS/stacking-analysis-dir/"
+
+sys.path.append(stacking_analysis_dir + 'codes/')
+import grid_coadd as gd
+
+if __name__ == '__main__':
+    
+    # PEARS data path
+    data_path = home + "/Documents/PEARS/data_spectra_only/"
+
+    # Read pears + 3dhst catalog
+    cat = np.genfromtxt(home + '/Desktop/FIGS/new_codes/color_stellarmass.txt',
+                       dtype=None, names=True, skip_header=2)
+
+    pears_id = cat['pearsid']
+    ur_color = cat['urcol']
+    stellarmass = cat['mstar']
+    photz = cat['threedzphot']
+
+    # Find indices for massive galaxies
+    massive_galaxies_indices = np.where(stellarmass >= 11.0)[0]
+
+    # Create grid for making grid plots
+    gs = gridspec.GridSpec(9, 18)
+    gs.update(left=0.1, right=0.9, bottom=0.1, top=0.9, wspace=0.1, hspace=0.1)
+
+    fig = plt.figure()
+
+    # Loop over all spectra 
+    for u in range(len(pears_id[massive_galaxies_indices])):
+
+        redshift = photz[massive_galaxies_indices][u]
+        lam_em, flam_em, ferr, specname = gd.fileprep(pears_id[massive_galaxies_indices][u], redshift)
+
+        row = 3 * int(u / 6)
+        col = 3 * int(u % 6)
+        ax = fig.add_subplot(gs[row:row+3, col:col+3])
+
+        ax.xaxis.set_ticklabels([])
+        ax.yaxis.set_ticklabels([])
+
+        if (row == 3) and (col == 0):
+            ax.set_ylabel(r'$f_{\lambda}\ [\mathrm{erg/s/cm^2/\AA}] $')
+        if (row == 6) and (col == 9):
+            ax.set_xlabel(r'$\lambda\ [\AA]$')
+
+        ax.plot(lam_em, flam_em, color='k')
+        ax.fill_between(lam_em, flam_em + ferr, flam_em - ferr, color='lightgray')
+
+        ax.set_xlim(2500, 6000)
+
+    fig.savefig(massive_figures_dir + 'massive_galaxies_spectra.eps', dpi=300)
+    #plt.show()
+
+    sys.exit(0)
+
