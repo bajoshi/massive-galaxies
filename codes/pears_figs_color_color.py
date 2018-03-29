6a8beac16f2070c1978f3377b0c762e3be4f02d2
Bhavin Joshi <bajoshi@asu.edu>
2018-03-28 23:30:54 -0700

    Small changes made to code that selects FIGS galaxies for MMIRS follow-up.

--- codes/pears_figs_color_color.py
+++ codes/pears_figs_color_color.py
@@ -39,43 +39,43 @@ if __name__ == '__main__':
     gn2_f160w_mag = -2.5 * np.log10(gn2cat['f160w_flux'] * 1e-9) + 8.9
 
     # Apply color cuts
-    color_cut_idx_gn1 = np.where((gn1_f160w_mag < 24) & (gn1_f105w_mag - gn1_f125w_mag >= 1.0))[0]
-    color_cut_idx_gn2 = np.where((gn2_f160w_mag < 24) & (gn2_f105w_mag - gn2_f125w_mag >= 1.0))[0]
+    color_cut_idx_gn1 = np.where((gn1_f160w_mag < 24) & (gn1_f105w_mag - gn1_f125w_mag >= 0.9))[0]
+    color_cut_idx_gn2 = np.where((gn2_f160w_mag < 24) & (gn2_f105w_mag - gn2_f125w_mag >= 0.9))[0]
 
     # make arrays of selected objects which passed the cuts
-    final_obj_figsid = []
-    final_obj_ra = []
-    final_obj_dec = []
-    final_obj_f160w_mag = []
-    final_obj_105_125_color = []
+    obj_figsid = []
+    obj_ra = []
+    obj_dec = []
+    obj_f160w_mag = []
+    obj_105_125_color = []
 
     for i in range(len(color_cut_idx_gn1)):
         f105_125_color = gn1_f105w_mag[color_cut_idx_gn1[i]] - gn1_f125w_mag[color_cut_idx_gn1[i]]
         if not np.isfinite(f105_125_color):
             continue
         else:
-            print gn1cat['id'][color_cut_idx_gn1[i]], gn1cat['ra'][color_cut_idx_gn1[i]], gn1cat['dec'][color_cut_idx_gn1[i]], \
-            gn1_f160w_mag[color_cut_idx_gn1[i]]
+            #print gn1cat['id'][color_cut_idx_gn1[i]], gn1cat['ra'][color_cut_idx_gn1[i]], gn1cat['dec'][color_cut_idx_gn1[i]], \
+            #gn1_f160w_mag[color_cut_idx_gn1[i]]
 
-            final_obj_figsid.append(gn1cat['id'][color_cut_idx_gn1[i]])
-            final_obj_ra.append(gn1cat['ra'][color_cut_idx_gn1[i]])
-            final_obj_dec.append(gn1cat['dec'][color_cut_idx_gn1[i]])
-            final_obj_f160w_mag.append(gn1_f160w_mag[color_cut_idx_gn1[i]])
-            final_obj_105_125_color.append(f105_125_color)
+            obj_figsid.append(gn1cat['id'][color_cut_idx_gn1[i]])
+            obj_ra.append(gn1cat['ra'][color_cut_idx_gn1[i]])
+            obj_dec.append(gn1cat['dec'][color_cut_idx_gn1[i]])
+            obj_f160w_mag.append(gn1_f160w_mag[color_cut_idx_gn1[i]])
+            obj_105_125_color.append(f105_125_color)
 
     for i in range(len(color_cut_idx_gn2)):
         f105_125_color = gn2_f105w_mag[color_cut_idx_gn2[i]] - gn2_f125w_mag[color_cut_idx_gn2[i]]
         if not np.isfinite(f105_125_color):
             continue
         else:
-            print gn2cat['id'][color_cut_idx_gn2[i]], gn2cat['ra'][color_cut_idx_gn2[i]], gn2cat['dec'][color_cut_idx_gn2[i]], \
-            gn2_f160w_mag[color_cut_idx_gn2[i]]
+            #print gn2cat['id'][color_cut_idx_gn2[i]], gn2cat['ra'][color_cut_idx_gn2[i]], gn2cat['dec'][color_cut_idx_gn2[i]], \
+            #gn2_f160w_mag[color_cut_idx_gn2[i]]
 
-            final_obj_figsid.append(gn2cat['id'][color_cut_idx_gn2[i]])
-            final_obj_ra.append(gn2cat['ra'][color_cut_idx_gn2[i]])
-            final_obj_dec.append(gn2cat['dec'][color_cut_idx_gn2[i]])
-            final_obj_f160w_mag.append(gn2_f160w_mag[color_cut_idx_gn2[i]])
-            final_obj_105_125_color.append(f105_125_color)
+            obj_figsid.append(gn2cat['id'][color_cut_idx_gn2[i]])
+            obj_ra.append(gn2cat['ra'][color_cut_idx_gn2[i]])
+            obj_dec.append(gn2cat['dec'][color_cut_idx_gn2[i]])
+            obj_f160w_mag.append(gn2_f160w_mag[color_cut_idx_gn2[i]])
+            obj_105_125_color.append(f105_125_color)
 
     # Match with MOSDEF catalog to make sure that they havent already taken data on this object
     # Match with Barger+ 2008 catalog to get Ks magnitudes whcih should be <21 for you to be able to obseve with MMIRS
@@ -117,13 +117,23 @@ if __name__ == '__main__':
     # Read in 3DHST catalog as well to check what the stellar masses are
     threed_v41_phot = fits.open(home + '/Desktop/FIGS/new_codes/3dhst_master.phot.v4.1/3dhst_master.phot.v4.1.cat.fits')
 
-    # Now match
-    for i in range(len(final_obj_ra)):
+    # Now match and take out the matches
+    # ONLY if they are in the MOSDEF catalog 
+    # and append to final arrays
+    final_obj_figsid = []
+    final_obj_ra = []
+    final_obj_dec = []
+    final_obj_f160w_mag = []
+    final_obj_105_125_color = []
+    final_obj_stellarmass = []
+    final_obj_zphot = []
 
-        current_ra = final_obj_ra[i]
-        current_dec = final_obj_dec[i]
-        current_id = final_obj_figsid[i]
-        current_f160w_mag = final_obj_f160w_mag[i]
+    for i in range(len(obj_ra)):
+
+        current_ra = obj_ra[i]
+        current_dec = obj_dec[i]
+        current_id = obj_figsid[i]
+        current_f160w_mag = obj_f160w_mag[i]
 
         # checked with a tolerance of 1.0 arcseconds as well
         # got the exact same result
@@ -138,13 +148,38 @@ if __name__ == '__main__':
             threed_v41_phot[1].data['dec'][threed_idx], current_dec, threed_v41_phot[1].data['z_peak'][threed_idx], \
             threed_v41_phot[1].data['lmass'][threed_idx], current_f160w_mag, current_id
 
-        if mosdef_idx.size:
-            print "MOSDEF match", mosdef_cat['RA'][mosdef_idx], current_ra, \
-            mosdef_cat['DEC'][mosdef_idx], current_dec, mosdef_cat['Z_MOSFIRE'][mosdef_idx], current_f160w_mag, current_id
-
         if barger_idx.size:
             print "BARGER match", barger_ra[barger_idx], current_ra, \
             barger_dec[barger_idx], current_dec, barger_zspec[barger_idx], barger_kmag[barger_idx], current_f160w_mag, current_id
+
+        if mosdef_idx.size:
+            #if float(mosdef_cat['Z_MOSFIRE'][mosdef_idx]) != -1.0:
+            print "MOSDEF match", mosdef_cat['RA'][mosdef_idx], current_ra, \
+            mosdef_cat['DEC'][mosdef_idx], current_dec, mosdef_cat['Z_MOSFIRE'][mosdef_idx], current_f160w_mag, current_id
+            continue
+
+        final_obj_figsid.append(current_id)
+        final_obj_ra.append(current_ra)
+        final_obj_dec.append(current_dec)
+        final_obj_f160w_mag.append(current_f160w_mag)
+        final_obj_105_125_color.append(obj_105_125_color[i])
+        final_obj_stellarmass.append(threed_v41_phot[1].data['lmass'][threed_idx])
+        final_obj_zphot.append(threed_v41_phot[1].data['z_peak'][threed_idx])
+
+    # Reprint object list to terminal
+    for i in range(len(final_obj_ra)):
+        print final_obj_figsid[i], final_obj_ra[i], final_obj_dec[i], final_obj_105_125_color[i], final_obj_f160w_mag[i], final_obj_stellarmass[i], final_obj_zphot[i]
+
+    # Plot RA dec on sky along with GOODS-N field and MMIRS MOS FoV overlaid
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.set_xlabel(r'$\mathrm{RA}$', fontsize=15)
+    ax.set_ylabel(r'$\mathrm{DEC}$', fontsize=15)   
+
+    ax.plot(final_obj_ra, final_obj_dec, 'o', markersize=2, color='k')
+
+    plt.show()
 
     sys.exit(0)
 
@@ -152,18 +187,23 @@ if __name__ == '__main__':
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    ax.plot(gn1_f160w_mag, gn1_f105w_mag - gn1_f125w_mag, 'o', color='k', markersize=2)
-    ax.scatter(gn1_f160w_mag[color_cut_idx_gn1], gn1_f105w_mag[color_cut_idx_gn1] - gn1_f125w_mag[color_cut_idx_gn1], s=50, edgecolors='r', facecolor='None')
+    ax.set_xlabel(r'$\mathrm{m_{AB}(F160W)}$', fontsize=15)
+    ax.set_ylabel(r'$\mathrm{m_{AB}(F105W) - m_{AB}(F125W)}$', fontsize=15)
 
-    plt.show()
+    # concatenate full gn1 and gn2 mags
+    all_f105w_mag = np.concatenate((gn1_f105w_mag, gn2_f105w_mag))
+    all_f125w_mag = np.concatenate((gn1_f125w_mag, gn2_f125w_mag))
+    all_f160w_mag = np.concatenate((gn1_f160w_mag, gn2_f160w_mag))
 
-    # -------------
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    ax.plot(all_f160w_mag, all_f105w_mag - all_f125w_mag, 'o', color='gray', markersize=1.5, alpha=0.4)
+    ax.scatter(final_obj_f160w_mag, final_obj_105_125_color, s=50, edgecolors='r', facecolor='None')
 
-    ax.plot(gn2_f160w_mag, gn2_f105w_mag - gn2_f125w_mag, 'o', color='k', markersize=2)
-    ax.scatter(gn2_f160w_mag[color_cut_idx_gn2], gn2_f105w_mag[color_cut_idx_gn2] - gn2_f125w_mag[color_cut_idx_gn2], s=50, edgecolors='r', facecolor='None')
+    ax.axhline(y=1.0, ls='--', lw=2, xmin=0, xmax=0.71)
+    ax.axvline(x=24.0, ls='--', lw=2, ymin=0.42, ymax=1.0)
 
-    plt.show()
+    ax.set_xlim(12,29)
+    ax.set_ylim(-0.5,3.0)
+
+    fig.savefig(home + '/Desktop/FIGS/massive-galaxies-figures/figs_105_125_160_colormag.png', dpi=150, bbox_inches='tight')
 
     sys.exit(0)
