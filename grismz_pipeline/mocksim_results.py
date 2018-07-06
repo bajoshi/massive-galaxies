f46dc152ca2bf5cf60c9b3b51df99d3c068669b0
Bhavin Joshi <bajoshi@asu.edu>
2018-07-06 14:38:42 -0400

    Updated plot for showing mock sim results

--- grismz_pipeline/mocksim_results.py
+++ grismz_pipeline/mocksim_results.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import numpy as np
+from scipy.optimize import curve_fit
 
 import sys
 import os
@@ -16,6 +17,121 @@ figs_dir = home + "/Desktop/FIGS/"
 def line_func(x, slope, intercept):
     return slope*x + intercept
 
+def plot_panel(ax_main, ax_resid, test_redshift_arr, mock_zgrism_arr, \
+    mock_zgrism_lowerr_arr, mock_zgrism_uperr_arr, d4000_range_lowlim):
+
+    # ------- z_mock vs test_redshift ------- #
+    ax_main.plot(test_redshift_arr, mock_zgrism_arr, 'o', markersize=2.0, color='k', markeredgecolor='k', zorder=10)
+    ax_main.plot(np.arange(0.2,1.5,0.01), np.arange(0.2,1.5,0.01), '--', color='r', linewidth=2.0)
+
+    ax_main.set_xlim(0.6, 1.24)
+    ax_main.set_ylim(0.6, 1.24)
+
+    ax_main.set_ylabel(r'$\mathrm{z_{mock}}$', fontsize=12)
+
+    ax_main.xaxis.set_ticklabels([])
+
+    # ------- residuals ------- #
+    ax_resid.plot(test_redshift_arr, (test_redshift_arr - mock_zgrism_arr)/(1+test_redshift_arr), 'o', \
+        markersize=2.0, color='k', markeredgecolor='k', zorder=10)
+    ax_resid.axhline(y=0, linestyle='--', color='r')
+
+    ax_resid.set_xlim(0.6, 1.24)
+
+    ax_resid.set_xlabel(r'$\mathrm{Test\ redshift}$', fontsize=12)
+    ax_resid.set_ylabel(r'$\mathrm{Residuals}$', fontsize=12, labelpad=-1)
+
+    # Turn on minorticks
+    ax_main.minorticks_on()
+    ax_resid.minorticks_on()
+
+    # Fitting
+    # do the fit with scipy
+    popt, pcov = curve_fit(line_func, test_redshift_arr, mock_zgrism_arr, p0=[1.0, 0.6])
+
+    # plot line fit
+    x_plot = np.arange(0.2,1.5,0.01)
+    ax_main.plot(x_plot, line_func(x_plot, popt[0], popt[1]), '-', color='#41ab5d', linewidth=2.0)
+
+    # Find residual stats
+    resid = (test_redshift_arr - mock_zgrism_arr)/(1+test_redshift_arr)
+    mu = np.mean(resid)
+    sigma_nmad = \
+    1.48 * np.median(abs(((test_redshift_arr - mock_zgrism_arr) - np.median(test_redshift_arr - mock_zgrism_arr)) / (1 + test_redshift_arr)))
+    stddev = np.std(resid)
+
+    outliers = np.where(resid > 0.05)[0]
+
+    print "\n", d4000_range_lowlim.replace('p', '.'), "<= D4000 <= 1.4"
+    print "Residual mean, sigma_nmad, and stddev:", mu, sigma_nmad, stddev
+    print "Number of outliers i.e. Residual>5%:", len(outliers)
+    out_frac = len(outliers)/len(mock_zgrism_arr)
+    print "Outlier fraction:", out_frac
+
+    # plot fit to residuals
+    ax_resid.axhline(y=mu + sigma_nmad, ls='-', color='#3690c0', linewidth=1.5)
+    ax_resid.axhline(y=mu - sigma_nmad, ls='-', color='#3690c0', linewidth=1.5)
+    ax_resid.axhline(y=mu, ls='-', color='blue', linewidth=1.5)
+
+    # tick labels
+    if d4000_range_lowlim == '1p35':
+        ax_resid.yaxis.set_ticklabels(['-0.1', '0', ''], fontsize='medium')
+    else:
+        ax_resid.yaxis.set_ticklabels(['', '-0.1', '0', ''], fontsize='medium')
+
+    if float(d4000_range_lowlim.replace('p', '.')) < 1.3:
+        ax_resid.xaxis.set_ticklabels([])
+        ax_resid.set_xlabel('')
+
+    # add text
+    ax_main.text(0.05, 0.95, d4000_range_lowlim.replace('p', '.') + r"$\ \leq \mathrm{D}4000 \leq 1.4$", \
+        verticalalignment='top', horizontalalignment='left', transform=ax_main.transAxes, color='k', size=12)
+    ax_main.text(0.05, 0.89, r'$\mathrm{\mu =\,}$' + convert_to_sci_not(mu), \
+        verticalalignment='top', horizontalalignment='left', transform=ax_main.transAxes, color='k', size=12)
+    ax_main.text(0.05, 0.84, r'$\mathrm{\sigma =\,}$' + convert_to_sci_not(stddev), \
+        verticalalignment='top', horizontalalignment='left', transform=ax_main.transAxes, color='k', size=12)
+
+    ax_main.text(0.05, 0.75, r'$\mathrm{N =\,}$' + str(len(mock_zgrism_arr)),\
+        verticalalignment='top', horizontalalignment='left', transform=ax_main.transAxes, color='k', size=12)
+    ax_main.text(0.05, 0.7, r'$\mathrm{Outlier\ fraction =\,}$' + "{:.3f}".format(out_frac),\
+        verticalalignment='top', horizontalalignment='left', transform=ax_main.transAxes, color='k', size=12)
+
+    # Plot avg mock_zgrism error bar on each panel
+    avg_lowerr = np.nanmean(mock_zgrism_lowerr_arr)
+    avg_uperr = np.nanmean(mock_zgrism_uperr_arr)
+    yerrbar = [[avg_lowerr], [avg_uperr]]
+
+    ax_main.errorbar(np.array([1.2]), np.array([0.7]), yerr=yerrbar, fmt='o', \
+        color='k', markeredgecolor='k', capsize=0, markersize=5.0, elinewidth=0.6)
+
+    return ax_main, ax_resid
+
+def convert_to_sci_not(n):
+    """
+    I'm not sure how well this function works
+    in every possible case. Needs more testing.
+    """
+
+    # convert to python string with sci notation
+    n_str = "{:.3e}".format(n)
+
+    # split string and assign parts
+    n_splt = n_str.split('e')
+    decimal = n_splt[0]
+    exponent = n_splt[1]
+
+    # strip leading zeros in exponent
+    if float(exponent) < 0:
+        exponent = exponent.split('-')[1]
+        exponent = '-' + exponent.lstrip('0')
+    elif float(exponent) > 0:
+        exponent = exponent.lstrip('0')
+
+    # create final string with proper TeX sci notation and return
+    sci_str = decimal + r'$\times$' + r'$\mathrm{10^{' + exponent + r'}}$'
+
+    return sci_str
+
 if __name__ == '__main__':
 
     # Read in results arrays
@@ -29,71 +145,60 @@ if __name__ == '__main__':
     mock_zgrism_higherr = np.load(massive_figures_dir + 'model_mockspectra_fits/mock_zgrism_higherr_list_1p2to1p4.npy')
 
     # --------- redshift accuracy comparison ---------- # 
-    fig = plt.figure()
-    gs = gridspec.GridSpec(10,10)
-    gs.update(left=0.05, right=0.95, bottom=0.05, top=0.95, wspace=0, hspace=0)
+    gs = gridspec.GridSpec(20,10)
+    gs.update(left=0.05, right=0.95, bottom=0.05, top=0.95, wspace=2.1, hspace=0.0)
 
-    ax1 = fig.add_subplot(gs[:8,:])
-    ax2 = fig.add_subplot(gs[8:,:])
+    fig_gs = plt.figure(figsize=(10, 10))
+    ax1 = fig_gs.add_subplot(gs[:8,:5])
+    ax2 = fig_gs.add_subplot(gs[8:10,:5])
+    
+    ax3 = fig_gs.add_subplot(gs[:8,5:])
+    ax4 = fig_gs.add_subplot(gs[8:10,5:])
+    
+    ax5 = fig_gs.add_subplot(gs[10:18,:5])
+    ax6 = fig_gs.add_subplot(gs[18:,:5])
 
-    # ------- z_mock vs test_redshift ------- #
-    ax1.plot(test_redshift, mock_zgrism, 'o', markersize=2.0, color='k', markeredgecolor='k', zorder=10)
-    ax1.plot(np.arange(0.2,1.5,0.01), np.arange(0.2,1.5,0.01), '--', color='r', linewidth=2.0)
+    ax7 = fig_gs.add_subplot(gs[10:18,5:])
+    ax8 = fig_gs.add_subplot(gs[18:,5:])
 
-    ax1.set_xlim(0.6, 1.24)
-    ax1.set_ylim(0.6, 1.24)
+    # Create arrays for all four panels
+    d4000_gtr_1p2_idx = np.where(d4000_out >= 1.2)[0]
+    d4000_gtr_1p25_idx = np.where(d4000_out >= 1.25)[0]
+    d4000_gtr_1p3_idx = np.where(d4000_out >= 1.3)[0]
+    d4000_gtr_1p35_idx = np.where(d4000_out >= 1.35)[0]
 
-    ax1.set_ylabel(r'$\mathrm{z_{mock}}$', fontsize=12)
+    test_redshift_d4000_gtr_1p2 = test_redshift[d4000_gtr_1p2_idx]
+    test_redshift_d4000_gtr_1p25 = test_redshift[d4000_gtr_1p25_idx]
+    test_redshift_d4000_gtr_1p3 = test_redshift[d4000_gtr_1p3_idx]
+    test_redshift_d4000_gtr_1p35 = test_redshift[d4000_gtr_1p35_idx]
 
-    ax1.xaxis.set_ticklabels([])
+    mock_zgrism_d4000_gtr_1p2 = mock_zgrism[d4000_gtr_1p2_idx]
+    mock_zgrism_d4000_gtr_1p25 = mock_zgrism[d4000_gtr_1p25_idx]
+    mock_zgrism_d4000_gtr_1p3 = mock_zgrism[d4000_gtr_1p3_idx]
+    mock_zgrism_d4000_gtr_1p35 = mock_zgrism[d4000_gtr_1p35_idx]
 
-    # ------- residuals ------- #
-    ax2.plot(test_redshift, (test_redshift - mock_zgrism)/(1+test_redshift), 'o', \
-        markersize=2.0, color='k', markeredgecolor='k', zorder=10)
-    ax2.axhline(y=0, linestyle='--', color='r')
+    mock_zgrism_lowerr_d4000_gtr_1p2 = mock_zgrism_lowerr[d4000_gtr_1p2_idx]
+    mock_zgrism_lowerr_d4000_gtr_1p25 = mock_zgrism_lowerr[d4000_gtr_1p25_idx]
+    mock_zgrism_lowerr_d4000_gtr_1p3 = mock_zgrism_lowerr[d4000_gtr_1p3_idx]
+    mock_zgrism_lowerr_d4000_gtr_1p35 = mock_zgrism_lowerr[d4000_gtr_1p35_idx]
 
-    ax2.set_xlim(0.6, 1.24)
+    mock_zgrism_higherr_d4000_gtr_1p2 = mock_zgrism_higherr[d4000_gtr_1p2_idx]
+    mock_zgrism_higherr_d4000_gtr_1p25 = mock_zgrism_higherr[d4000_gtr_1p25_idx]
+    mock_zgrism_higherr_d4000_gtr_1p3 = mock_zgrism_higherr[d4000_gtr_1p3_idx]
+    mock_zgrism_higherr_d4000_gtr_1p35 = mock_zgrism_higherr[d4000_gtr_1p35_idx]
 
-    ax2.set_xticklabels([])
+    # make panel plots
+    ax1, ax2 = plot_panel(ax1, ax2, test_redshift_d4000_gtr_1p2, mock_zgrism_d4000_gtr_1p2, \
+        mock_zgrism_lowerr_d4000_gtr_1p2, mock_zgrism_higherr_d4000_gtr_1p2, '1p2')
+    ax3, ax4 = plot_panel(ax3, ax4, test_redshift_d4000_gtr_1p25, mock_zgrism_d4000_gtr_1p25, \
+        mock_zgrism_lowerr_d4000_gtr_1p25, mock_zgrism_higherr_d4000_gtr_1p25, '1p25')
+    ax5, ax6 = plot_panel(ax5, ax6, test_redshift_d4000_gtr_1p3, mock_zgrism_d4000_gtr_1p3, \
+        mock_zgrism_lowerr_d4000_gtr_1p3, mock_zgrism_higherr_d4000_gtr_1p3, '1p3')
+    ax7, ax8 = plot_panel(ax7, ax8, test_redshift_d4000_gtr_1p35, mock_zgrism_d4000_gtr_1p35, \
+        mock_zgrism_lowerr_d4000_gtr_1p35, mock_zgrism_higherr_d4000_gtr_1p35, '1p35')
 
-    ax2.set_xlabel(r'$\mathrm{Test\ redshift}$', fontsize=12)
-    ax2.set_ylabel(r'$\mathrm{Residuals}$', fontsize=12, labelpad=-1)
-
-    # Turn on minorticks
-    ax1.minorticks_on()
-    ax2.minorticks_on()
-
-    # Fitting
-    # do the fit with scipy
-    from scipy.optimize import curve_fit
-    popt, pcov = curve_fit(line_func, test_redshift, mock_zgrism, p0=[1.0, 0.6])
-
-    # plot line fit
-    x_plot = np.arange(0.2,1.5,0.01)
-    ax1.plot(x_plot, line_func(x_plot, popt[0], popt[1]), '-', color='#41ab5d', linewidth=2.0)
-
-    # Find stddev for the residuals
-    resid = (test_redshift - mock_zgrism)/(1+test_redshift)
-    mu = np.mean(resid)
-    sigma_nmad = 1.48 * np.median(abs(((test_redshift - mock_zgrism) - np.median((test_redshift - mock_zgrism))) / (1 + test_redshift)))
-
-    print "Mean and sigma_nmad:", mu, sigma_nmad
-
-    # plot fit to residuals
-    ax2.axhline(y=mu + sigma_nmad, ls='-', color='#3690c0', linewidth=1.5)
-    ax2.axhline(y=mu - sigma_nmad, ls='-', color='#3690c0', linewidth=1.5)
-    ax2.axhline(y=mu, ls='-', color='blue', linewidth=1.5)
-
-    # tick labels
-    ax2.yaxis.set_ticklabels(['', '-0.1', '0', ''], fontsize='medium')
-
-    # add text
-    ax1.text(0.05, 0.9, r'$\mathrm{\mu=}$' + "{:.3}".format(mu), \
-        verticalalignment='top', horizontalalignment='left', transform=ax1.transAxes, color='k', size=12)
-    ax1.text(0.05, 0.85, r'$\mathrm{\sigma_{NMAD}=}$' + "{:.3}".format(sigma_nmad), \
-        verticalalignment='top', horizontalalignment='left', transform=ax1.transAxes, color='k', size=12)
-
-    fig.savefig(massive_figures_dir + \
+    # Save figure 
+    fig_gs.savefig(massive_figures_dir + \
         'model_mockspectra_fits/mock_redshift_comparison_d4000_1p2to1p4.eps', dpi=300, bbox_inches='tight')
 
     sys.exit(0)
