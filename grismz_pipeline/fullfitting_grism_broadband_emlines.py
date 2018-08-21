e1c3820e67b97f1f9e45cb763cafd0d96bfa6160
Bhavin Joshi <bajoshi@asu.edu>
2018-08-20 17:20:29 -0700

    Added ACS zeropoints. These are the outdated ones for now.

--- grismz_pipeline/fullfitting_grism_broadband_emlines.py
+++ grismz_pipeline/fullfitting_grism_broadband_emlines.py
@@ -45,18 +45,24 @@ def check_spec_plot(grism_spec, grism_lam_obs, grism_ferr_obs):
 
 def get_filt_zp(filtname):
 
-    filtname_arr = np.array(['F125W', 'F140W', 'F160W'])  # 'F435W', 'F606W', 'F775W', 'F850LP', 
+    filtname_arr = np.array(['F435W', 'F606W', 'F775W', 'F850LP', 'F125W', 'F140W', 'F160W']) 
 
     # Corresponding lists containing AB and ST zeropoints
     # The first 4 filters i.e. 'F435W', 'F606W', 'F775W', 'F850LP'
-    # are ACS/WFC filters and the ZP calculation is yet to be done for these.
+    # are ACS/WFC filters and the correct ZP calculation is yet to be done for these.
     # ACS zeropoints are time-dependent and therefore the zeropoint calculator has to be used.
     # Check: http://www.stsci.edu/hst/acs/analysis/zeropoints
     # For WFC3/IR: http://www.stsci.edu/hst/wfc3/analysis/ir_phot_zpt
-    filt_zp_st_list = [28.0203, 28.479, 28.1875]
-    filt_zp_ab_list = [26.2303, 26.4524, 25.9463]
+    # This page: http://www.stsci.edu/hst/acs/analysis/zeropoints/old_page/localZeropoints
+    # gives the old ACS zeropoints.
+    # I'm going to use the old zeropoints for now.
+    # While these are outdated, I have no way of knowing the dates of every single GOODS observation 
+    # (actually I could figure out the dates from the archive) and then using the zeropoint associated 
+    # with that date and somehow combining all the photometric data to get some average zeropoint?
+    filt_zp_st_list = [25.16823, 26.67444, 26.41699, 25.95456, 28.0203, 28.479, 28.1875]
+    filt_zp_ab_list = [25.68392, 26.50512, 25.67849, 24.86663, 26.2303, 26.4524, 25.9463]
 
-    filt_idx = np.where(filtname_arr == filtname)[0]
+    filt_idx = np.where(filtname_arr == filtname)[0][0]
 
     filt_zp_st = filt_zp_st_list[filt_idx]
     filt_zp_ab = filt_zp_ab_list[filt_idx]
@@ -68,12 +74,13 @@ def get_flam(filtname, cat_flux):
 
     filt_zp_st, filt_zp_ab = get_filt_zp(filtname)
 
-    abmag = 25.0 - 2.5*np.log10(cat_flux)
+    cat_flux = float(cat_flux)  # because it should be a single float
+    abmag = 25.0 - 2.5*np.log10(cat_flux)  # 3DHST fluxes are normalized to an ABmag ZP of 25.0
     stmag = filt_zp_st - filt_zp_ab + abmag
 
     flam = 10**(-1 * (stmag + 21.1) / 2.5)
 
-    return stmag
+    return flam
 
 if __name__ == '__main__':
     
@@ -113,16 +120,41 @@ if __name__ == '__main__':
         current_ra = float(matched_cat_n['pearsra'][cat_idx])
         current_dec = float(matched_cat_n['pearsdec'][cat_idx])
 
+    # Now match
     ra_lim = 0.5/3600  # arcseconds in degrees
     dec_lim = 0.5/3600
     threed_phot_idx = np.where((threed_ra >= current_ra - ra_lim) & (threed_ra <= current_ra + ra_lim) & \
         (threed_dec >= current_dec - dec_lim) & (threed_dec <= current_dec + dec_lim))[0]
 
-    print get_flam('F125W', goodsn_phot_cat_3dhst['f_F125W'][threed_phot_idx])
-    print get_flam('F140W', goodsn_phot_cat_3dhst['f_F140W'][threed_phot_idx])
-    print get_flam('F160W', goodsn_phot_cat_3dhst['f_F160W'][threed_phot_idx])
+    # Get photometric fluxes and their errors
+    flam_f435w = get_flam('F435W', goodsn_phot_cat_3dhst['f_F435W'][threed_phot_idx])
+    flam_f606w = get_flam('F606W', goodsn_phot_cat_3dhst['f_F606W'][threed_phot_idx])
+    flam_f775w = get_flam('F775W', goodsn_phot_cat_3dhst['f_F775W'][threed_phot_idx])
+    flam_f850lp = get_flam('F850LP', goodsn_phot_cat_3dhst['f_F850LP'][threed_phot_idx])
+    flam_f125w = get_flam('F125W', goodsn_phot_cat_3dhst['f_F125W'][threed_phot_idx])
+    flam_f140w = get_flam('F140W', goodsn_phot_cat_3dhst['f_F140W'][threed_phot_idx])
+    flam_f160w = get_flam('F160W', goodsn_phot_cat_3dhst['f_F160W'][threed_phot_idx])
+
+    ferr_f435w = get_flam('F435W', goodsn_phot_cat_3dhst['e_F435W'][threed_phot_idx])
+    ferr_f606w = get_flam('F606W', goodsn_phot_cat_3dhst['e_F606W'][threed_phot_idx])
+    ferr_f775w = get_flam('F775W', goodsn_phot_cat_3dhst['e_F775W'][threed_phot_idx])
+    ferr_f850lp = get_flam('F850LP', goodsn_phot_cat_3dhst['e_F850LP'][threed_phot_idx])
+    ferr_f125w = get_flam('F125W', goodsn_phot_cat_3dhst['e_F125W'][threed_phot_idx])
+    ferr_f140w = get_flam('F140W', goodsn_phot_cat_3dhst['e_F140W'][threed_phot_idx])
+    ferr_f160w = get_flam('F160W', goodsn_phot_cat_3dhst['e_F160W'][threed_phot_idx])
+
+    print flam_f435w, ferr_f435w
+    print flam_f606w, ferr_f606w
+    print flam_f775w, ferr_f775w
+    print flam_f850lp, ferr_f850lp
+    print flam_f125w, ferr_f125w
+    print flam_f140w, ferr_f140w
+    print flam_f160w, ferr_f160w
+
+    phot_fluxes_arr = np.array([flam_f435w, flam_f606w, flam_f775w, flam_f850lp, flam_f125w, flam_f140w, flam_f160w])
+    phot_errors_arr = np.array([ferr_f435w, ferr_f606w, ferr_f775w, ferr_f850lp, ferr_f125w, ferr_f140w, ferr_f160w])
 
     # Plot to check
-    #check_spec_plot(spec, lam_obs, ferr_obs, )
+    check_spec_plot(spec, lam_obs, ferr_obs, phot_fluxes_arr, phot_errors_arr)
 
     sys.exit(0)
