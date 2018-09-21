03c8bc308c7eb8d0e96256daa14f873af38e006b
Bhavin Joshi <bajoshi@asu.edu>
2018-09-21 14:56:38 -0700

    Added contamination checking to zweight_results.py

--- grismz_pipeline/zweight_results.py
+++ grismz_pipeline/zweight_results.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import numpy as np
+from astropy.io import fits
 
 import glob
 import os
@@ -13,6 +14,56 @@ import matplotlib as mpl
 home = os.getenv('HOME')
 massive_galaxies_dir = home + "/Desktop/FIGS/massive-galaxies/"
 massive_figures_dir = home + "/Desktop/FIGS/massive-galaxies-figures/"
+stacking_analysis_dir = home + "/Desktop/FIGS/stacking-analysis-pears/"
+
+sys.path.append(stacking_analysis_dir + 'codes/')
+sys.path.append(massive_galaxies_dir + 'grismz_pipeline/')
+import grid_coadd as gd
+import mocksim_results as mr
+
+def get_contam(pears_index, field):
+
+    # read in spectrum file
+    data_path = home + "/Documents/PEARS/data_spectra_only/"
+    # Get the correct filename and the number of extensions
+    if field == 'GOODS-N':
+        filename = data_path + 'h_pears_n_id' + str(pears_index) + '.fits'
+    elif field == 'GOODS-S':
+        filename = data_path + 'h_pears_s_id' + str(pears_index) + '.fits'
+
+    fitsfile = fits.open(filename)
+    n_ext = fitsfile[0].header['NEXTEND']
+
+    # Loop over all extensions and get the best PA
+    # Get highest netsig to find the spectrum to be added
+    if n_ext > 1:
+        netsiglist = []
+        palist = []
+        for count in range(n_ext):
+            #print "At PA", fitsfile[count+1].header['POSANG']  # Line useful for debugging. Do not remove. Just uncomment.
+            fitsdata = fitsfile[count+1].data
+            netsig = gd.get_net_sig(fitsdata)
+            netsiglist.append(netsig)
+            palist.append(fitsfile[count+1].header['POSANG'])
+            #print "At PA", fitsfile[count+1].header['POSANG'], "with NetSig", netsig  
+            # Above line also useful for debugging. Do not remove. Just uncomment.
+        netsiglist = np.array(netsiglist)
+        maxnetsigarg = np.argmax(netsiglist)
+        netsig_chosen = np.max(netsiglist)
+        spec_toadd = fitsfile[maxnetsigarg+1].data
+        pa_chosen = fitsfile[maxnetsigarg+1].header['POSANG']
+    elif n_ext == 1:
+        spec_toadd = fitsfile[1].data
+        pa_chosen = fitsfile[1].header['POSANG']
+        netsig_chosen = gd.get_net_sig(fitsfile[1].data)
+        
+    # Now get the spectrum to be added
+    lam_obs = spec_toadd['LAMBDA']
+    flam_obs = spec_toadd['FLUX']
+    ferr_obs = spec_toadd['FERROR']
+    contam = spec_toadd['CONTAM']
+
+    return lam_obs, flam_obs, ferr_obs, contam
 
 if __name__ == '__main__':
 
@@ -40,6 +91,7 @@ if __name__ == '__main__':
     all_zwt_2xgrismerr = []
     all_zwt_2xphoterr = []
     all_d4000 = []
+    all_contam_frac = []
 
     # loop over all galaxies that the code has run through
     fl_count = 0
@@ -91,7 +143,13 @@ if __name__ == '__main__':
         current_d4000 = d4000_arr[results_idx]
 
         all_d4000.append(current_d4000)
-        
+
+        # Get contamination
+        lam_obs, flam_obs, ferr_obs, contam = get_contam(current_id, current_field)
+
+        all_contam_frac.append(np.nansum(contam) / np.nansum(flam_obs))
+
+        # -------------------------------------
         # Get weighted redshifts for other runs
         # define paths
         grismerr_2x_fl_path = fl.replace('large_diff_specz_sample/run_with_1xerrors', 'large_diff_specz_sample/run_with_2xgrismerrors')
@@ -127,10 +185,13 @@ if __name__ == '__main__':
     all_zwt_2xgrismerr = np.asarray(all_zwt_2xgrismerr)
     all_zwt_2xphoterr = np.asarray(all_zwt_2xphoterr)
     all_d4000 = np.asarray(all_d4000)
+    all_contam_frac = np.asarray(all_contam_frac)
 
     # -------------------------------------------- Quantify results -------------------------------------------- #
     # Cut on D4000
-    d4000_idx = np.where((all_d4000 >= 1.1) & (all_d4000 < 1.6))[0]
+    d4000_low = 1.1
+    d4000_high = 1.6
+    d4000_idx = np.where((all_d4000 >= d4000_low) & (all_d4000 < d4000_high))[0]
     print "Galaxies in D4000 range:", len(d4000_idx)
 
     # apply cuts
@@ -142,6 +203,7 @@ if __name__ == '__main__':
     all_zwt_2xgrismerr = all_zwt_2xgrismerr[d4000_idx]
     all_zwt_2xphoterr = all_zwt_2xphoterr[d4000_idx]
     all_d4000 = all_d4000[d4000_idx]
+    all_contam_frac = all_contam_frac[d4000_idx]
 
     # Get residuals
     resid_photoz = (all_specz - all_photoz) / (1+all_specz)
@@ -165,12 +227,32 @@ if __name__ == '__main__':
     print "Mean, std. dev., and sigma_NMAD for residual weighted z (2xgrismerr):", "{:.4}".format(np.mean(resid_zweight_2xgrismerr)), "{:.4}".format(np.std(resid_zweight_2xgrismerr)), "{:.4}".format(sigma_nmad_zwt_2xgrismerr)
     print "Mean, std. dev., and sigma_NMAD for residual weighted z (2xphoterr):", "{:.4}".format(np.mean(resid_zweight_2xphoterr)), "{:.4}".format(np.std(resid_zweight_2xphoterr)), "{:.4}".format(sigma_nmad_zwt_2xphoterr)
 
-    # Plot 
+    # -------------------------------------------- Plotting -------------------------------------------- #
+    # ---------------------- Residual histogram ---------------------- #
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    ax.hist(resid_photoz, 30, color='r', histtype='step', lw=2, label='Photo-z residuals', range=(-0.1, 0.1))
-    ax.hist(resid_zweight_1xerr, 30, color='b', histtype='step', lw=2, label='SPZ residuals', range=(-0.1, 0.1))
+    ax.set_xlabel(r'Contamination Fraction')
+    ax.set_ylabel(r'Abs(Residuals [SPZ])')
+
+    ax.scatter(all_contam_frac, abs(resid_zweight_1xerr), s=5)
+
+    ax.set_xlim(min(all_contam_frac), max(all_contam_frac))
+
+    ax.axhline(y=0.0, color='k', ls='--')
+
+    fig.savefig(massive_figures_dir + 'large_diff_specz_sample/resid_contam.png', dpi=300, bbox_inches='tight')
+
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+    # ---------------------- Residual histogram ---------------------- #
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.hist(resid_photoz, 20, color='r', histtype='step', lw=2, label='Photo-z residuals', range=(-0.1, 0.1))
+    ax.hist(resid_zweight_1xerr, 20, color='b', histtype='step', lw=2, label='SPZ residuals', range=(-0.1, 0.1))
     #ax.hist(resid_zweight_2xgrismerr, color='g', histtype='step', lw=2)#, range=(-0.12, 0.12))
     #ax.hist(resid_zweight_2xphoterr, color='orange', histtype='step', lw=2)#, range=(-0.12, 0.12))
 
@@ -179,15 +261,38 @@ if __name__ == '__main__':
     ax.set_xlabel(r'$\mathrm{Residuals}$')
     ax.set_ylabel(r'$\mathrm{N}$')
 
-    ax.text(0.05, 0.85, r'$\mathrm{1.1\leq D4000 < 1.6}$', \
-    verticalalignment='top', horizontalalignment='left', \
-    transform=ax.transAxes, color='k', size=14)
-
     ax.minorticks_on()
     ax.legend(loc='upper left')
 
-    fig.savefig(massive_figures_dir + 'large_diff_specz_sample/resid_hist.png', dpi=300, bbox_inches='tight')
+    # Info text
+    ax.text(0.05, 0.85, r'$' + str(d4000_low) + '\leq \mathrm{D4000} < ' + str(d4000_high) + '$', \
+    verticalalignment='top', horizontalalignment='left', \
+    transform=ax.transAxes, color='k', size=14)
 
-    plt.show()
+    ax.text(0.05, 0.8, r'$\mathrm{N = }$' + str(len(d4000_idx)), \
+    verticalalignment='top', horizontalalignment='left', \
+    transform=ax.transAxes, color='k', size=14)
+
+
+    ax.text(0.05, 0.71, r'$\mu_{\mathrm{photo-z}} = $' + mr.convert_to_sci_not(np.mean(resid_photoz)), \
+    verticalalignment='top', horizontalalignment='left', \
+    transform=ax.transAxes, color='k', size=10)
+    ax.text(0.05, 0.66, r'$\mathrm{\sigma^{NMAD}_{photo-z}} = $' + mr.convert_to_sci_not(sigma_nmad_photo), \
+    verticalalignment='top', horizontalalignment='left', \
+    transform=ax.transAxes, color='k', size=10)
+
+    ax.text(0.05, 0.58, r'$\mu_{\mathrm{SPZ}} = $' + mr.convert_to_sci_not(np.mean(resid_zweight_1xerr)), \
+    verticalalignment='top', horizontalalignment='left', \
+    transform=ax.transAxes, color='k', size=10)
+    ax.text(0.05, 0.53, r'$\mathrm{\sigma^{NMAD}_{SPZ}} = $' + mr.convert_to_sci_not(sigma_nmad_zwt_1xerr), \
+    verticalalignment='top', horizontalalignment='left', \
+    transform=ax.transAxes, color='k', size=10)
+
+    # Save figure
+    fig.savefig(massive_figures_dir + \
+        'large_diff_specz_sample/resid_hist_' + 'd4000_' + str(d4000_low) + 'to' + str(d4000_high) + '.png', \
+        dpi=300, bbox_inches='tight')
+
+    #plt.show()
 
     sys.exit(0)
