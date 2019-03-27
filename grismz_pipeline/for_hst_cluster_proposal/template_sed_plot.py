c4c7e3dc13d0d4b7967b788e665fff553238bf5c
Bhavin Joshi <bajoshi@asu.edu>
2019-03-27 12:44:57 -0700

    Starting code to make SED template figure for Brenda's HST proposal

---
+++ grismz_pipeline/for_hst_cluster_proposal/template_sed_plot.py
@@ -0,0 +1,31 @@
+from __future__ import division
+
+import numpy as np
+from astropy.io import fits
+from scipy.interpolate import griddata
+
+import os
+import sys
+import time
+import datetime
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib
+
+home = os.getenv('HOME')
+massive_galaxies_dir = home + "/Desktop/FIGS/massive-galaxies/"
+
+def plot_template_sed():
+
+	return None
+
+def main():
+
+	# Read in the filters from Seth
+
+	return None
+
+if __name__ == '__main__':
+	main()
+	sys.exit(0)
