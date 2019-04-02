7b752dd45edd1c78c7c7bde19bb9bd30bf9133a2
Bhavin Joshi <bajoshi@asu.edu>
2019-04-02 14:47:19 -0700

    Added more code for G165 proposal plots

--- grismz_pipeline/for_hst_cluster_proposal/template_sed_plot.py
+++ grismz_pipeline/for_hst_cluster_proposal/template_sed_plot.py
@@ -14,6 +14,7 @@ import datetime
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 home = os.getenv('HOME')
 massive_galaxies_dir = home + "/Desktop/FIGS/massive-galaxies/"
@@ -103,12 +104,12 @@ def get_model_grism_spec(template, grism_resamp_grid, grism_curve):
 
     return model_grism_spec
 
-def chop_grism_spec(g141, resampling_lam_grid, grism_spec):
+def chop_grism_spec(grism_curve, resampling_lam_grid, grism_spec):
 
     # Now chop the grism curve to where the transmission is above 10%
-    grism_idx = np.where(g141['trans'] >= 0.1)[0]
-    low_grism_lim = g141['wav'][grism_idx][0]
-    high_grism_lim = g141['wav'][grism_idx][-1]
+    grism_idx = np.where(grism_curve['trans'] >= 0.1)[0]
+    low_grism_lim = grism_curve['wav'][grism_idx][0]
+    high_grism_lim = grism_curve['wav'][grism_idx][-1]
 
     low_idx = np.argmin(abs(resampling_lam_grid - low_grism_lim))
     high_idx = np.argmin(abs(resampling_lam_grid - high_grism_lim))
@@ -149,7 +150,8 @@ def get_dust_atten_model(model_wav_arr, model_flux_arr, av):
 
     return dust_atten_model_flux
 
-def plot_template_sed(model, model_name, phot_lam, model_photometry, resampling_lam_grid, model_grism_spec, all_filters, filter_names):
+def plot_template_sed(model, model_name, phot_lam, model_photometry, resampling_lam_grid, model_grism_spec, \
+    all_filters, filter_names, grism_name):
 
     # --------- Plot --------- # 
     fig = plt.figure()
@@ -163,14 +165,20 @@ def plot_template_sed(model, model_name, phot_lam, model_photometry, resampling_
     # need twinx first
     ax1 = ax.twinx()
     for filt in all_filters:
-        ax1.plot(filt['wav'], filt['trans'], zorder=2)
+        ax1.plot(filt['wav'], filt['trans'], zorder=4)
 
     # Twin axis related stuff
     ax1.set_ylabel(r'$\mathrm{Transmission}$', fontsize=15)
-    ax1.set_ylim(0, 1.05)
+    ax1.set_ylim(0, 1.15)
 
     # plot template (along with the dust attenuated one if required)
-    ax.plot(model['wav'], model['flam_norm'], color='k', zorder=4)
+    if grism_name == 'G102':
+        # Plot smoothed spectrum if using high-res BC03
+        from astropy.convolution import convolve, Gaussian1DKernel
+        kernel = Gaussian1DKernel(stddev=5)
+        ax.plot(model['wav'], convolve(model['flam_norm'], kernel), color='k', zorder=2)
+    else:
+        ax.plot(model['wav'], model['flam_norm'], color='k', zorder=2)
     # If you're working with Mrk231 then also show the dust-attenuated SEDs
     if model_name == 'Mrk 231':
         dust_atten_model_flux_av1 = get_dust_atten_model(model['wav'], model['flam_norm'], 1.0)
@@ -188,17 +196,47 @@ def plot_template_sed(model, model_name, phot_lam, model_photometry, resampling_
     ax.plot(phot_lam, model_photometry, 'o', color='r', markersize=6, zorder=6)
 
     # Plot simulated grism spectrum
-    ax.plot(resampling_lam_grid, model_grism_spec, 'o-', markersize=2, color='seagreen', lw=4, zorder=5)
+    # Take out NaN values so it connects all the dots
+    nonan_idx = np.where(~np.isnan(model_grism_spec))
+    resampling_lam_grid_nonan = resampling_lam_grid[nonan_idx]
+    model_grism_spec_nonan = model_grism_spec[nonan_idx]
+    ax.plot(resampling_lam_grid_nonan, model_grism_spec_nonan, 'o-', markersize=2, color='seagreen', lw=3.5, zorder=5)
+
+    # Also show the grism only spectrum as an inset plot
+    ax_in = inset_axes(ax, width="35%", height="22%", loc=1)
+    ax_in.plot(resampling_lam_grid_nonan, model_grism_spec_nonan, 'o-', markersize=2, color='seagreen', lw=1.5, zorder=5)
+    ax_in.minorticks_on()
+    ax_in.set_yticklabels([])
+
+    # Show labels for emission lnies
+    if 'Ell' in model_name:
+        ax_in.axvline(x=8860, ls='--', color='r', ymin=0.25, ymax=0.6)
+        ax_in.text(0.2, 0.23, r'H${\alpha}$', verticalalignment='top', horizontalalignment='left', \
+        transform=ax_in.transAxes, color='k', size=10)
+        ax_in.text(0.45, 0.95, 'G102 spectrum', verticalalignment='top', horizontalalignment='left', \
+        transform=ax_in.transAxes, color='k', size=10)
+    elif 'SF' in model_name:
+        ax_in.axvline(x=11181, ls='--', color='r', ymin=0.2, ymax=0.8)
+        ax_in.text(0.46, 0.53, r'${\rm [OII]}\lambda 3727$', verticalalignment='top', horizontalalignment='left', \
+        transform=ax_in.transAxes, color='k', size=10)
+        ax_in.text(0.05, 0.95, 'G102 spectrum', verticalalignment='top', horizontalalignment='left', \
+        transform=ax_in.transAxes, color='k', size=10)
 
     # Fix all zorder
-    ax.set_zorder(ax1.get_zorder()+1) # put ax in front of ax1 
-    ax.patch.set_visible(False) # hide the 'canvas'
+    if grism_name == 'G141':
+        ax.set_zorder(ax1.get_zorder()+1) # put ax in front of ax1 
+        ax.patch.set_visible(False) # hide the 'canvas'
 
     # Other formatting stuff
-    ax.set_xlim(0.3e4, 10e4)
-    ax.set_ylim(-0.05, 1.45)
+    if grism_name == 'G141':
+        ax.set_ylim(-0.05, 1.45)
 
+    ax.set_xlim(0.3e4, 10e4)
     ax.set_xscale('log')
+
+    if (grism_name == 'G102') and ('SF' in model_name):
+        ax.set_yscale('log')
+        ax.set_ylim(0.01, 100)
 
     ax.minorticks_on()
 
@@ -213,7 +251,7 @@ def plot_template_sed(model, model_name, phot_lam, model_photometry, resampling_
 
     # Text for info
     # Template name
-    ax.text(0.5, 0.96, model_name, verticalalignment='top', horizontalalignment='left', \
+    ax.text(0.02, 0.98, model_name, verticalalignment='top', horizontalalignment='left', \
     transform=ax.transAxes, color='k', size=17)
 
     # Other info 
@@ -237,41 +275,122 @@ def plot_template_sed(model, model_name, phot_lam, model_photometry, resampling_
     ax.text(0.38, 0.05, filter_names[3], verticalalignment='top', horizontalalignment='left', \
     transform=ax.transAxes, color='k', size=12, zorder=10)
     """
-
+    
+    #plt.show()
     savename = 'sed_plot_' + model_name.replace(' ', '_') + '.pdf'
     fig.savefig(savename, dpi=300, bbox_inches='tight')
 
     return None
 
-def do_all_mods_plot(template, template_name, all_filters, filter_names, phot_lam, g141):
+def do_all_mods_plot(template, template_name, all_filters, filter_names, phot_lam, grism_curve, grism_name):
 
     template_flam_list = []
     for i in range(len(all_filters)):
         template_flam_list.append(get_model_photometry(template, all_filters[i]))
 
     # Create grism resampling grid
-    # Get an example observed G141 spectrum from 3D-HST
-    example_g141_hdu = fits.open('goodsn-16-G141_33780.1D.fits')
-    grism_lam_obs = example_g141_hdu[1].data['wave']
+    if grism_name == 'G141':
+        # Get an example observed G141 spectrum from 3D-HST
+        example_g141_hdu = fits.open('goodsn-16-G141_33780.1D.fits')
+        grism_lam_obs = example_g141_hdu[1].data['wave']
 
-    # extend lam_grid to be able to move the lam_grid later 
-    avg_dlam = old_ref.get_avg_dlam(grism_lam_obs)
+        # extend lam_grid to be able to move the lam_grid later 
+        avg_dlam = old_ref.get_avg_dlam(grism_lam_obs)
 
-    lam_low_to_insert = np.arange(10000, grism_lam_obs[0], avg_dlam, dtype=np.float64)
-    lam_high_to_append = np.arange(grism_lam_obs[-1] + avg_dlam, 17800, avg_dlam, dtype=np.float64)
+        grism_low_lim = 10000
+        grism_high_lim = 17800
+
+    elif grism_name == 'G102':
+        # Get an example observed G102 spectrum from FIGS
+        example_g102_hdu = fits.open(figs_dir + 'spc_files/GS1_G102_2.combSPC.fits')
+        grism_lam_obs = example_g102_hdu[1].data['LAMBDA']
+
+        # extend lam_grid to be able to move the lam_grid later 
+        avg_dlam = old_ref.get_avg_dlam(grism_lam_obs)
+
+        grism_low_lim = 7600
+        grism_high_lim = 13000
+
+    lam_low_to_insert = np.arange(grism_low_lim, grism_lam_obs[0], avg_dlam, dtype=np.float64)
+    lam_high_to_append = np.arange(grism_lam_obs[-1] + avg_dlam, grism_high_lim, avg_dlam, dtype=np.float64)
 
     resampling_lam_grid = np.insert(grism_lam_obs, obj=0, values=lam_low_to_insert)
     resampling_lam_grid = np.append(resampling_lam_grid, lam_high_to_append)
 
-    template_grism_spec = get_model_grism_spec(template, resampling_lam_grid, g141)
-
-    resampling_lam_grid_short, template_grism_spec_short = chop_grism_spec(g141, resampling_lam_grid, template_grism_spec)
+    template_grism_spec = get_model_grism_spec(template, resampling_lam_grid, grism_curve)
+    resampling_lam_grid_short, template_grism_spec_short = chop_grism_spec(grism_curve, resampling_lam_grid, template_grism_spec)
 
     # ---------------------------------- Plot ---------------------------------- #
     plot_template_sed(template, template_name, phot_lam, template_flam_list, \
-        resampling_lam_grid_short, template_grism_spec_short, all_filters, filter_names)
+        resampling_lam_grid_short, template_grism_spec_short, all_filters, filter_names, grism_name)
 
     return None
+
+def get_template(age, tau, tauv, metallicity, \
+    log_age_arr, metal_arr, tau_gyr_arr, tauv_arr, \
+    model_lam_grid_withlines_mmap, model_comp_spec_withlines_mmap, plotcheck=False):
+
+    print "\n", "Finding closest model to --"
+    print "Age [Gyr]:", 10**age / 1e9
+    print "Tau [Gyr]:", tau
+    print "Tau_v:", tauv
+    print "Metallicity [abs. frac.]:", metallicity
+
+    # First find closest values and then indices corresponding to them
+    # It has to be done this way because 
+    closest_age_idx = np.argmin(abs(log_age_arr - age))
+    closest_tau_idx = np.argmin(abs(tau_gyr_arr - tau))
+
+    # Now get indices
+    age_idx = np.where(log_age_arr == log_age_arr[closest_age_idx])[0]
+    tau_idx = np.where(tau_gyr_arr == tau_gyr_arr[closest_tau_idx])[0]
+    tauv_idx = np.where(tauv_arr == tauv)[0]
+    metal_idx = np.where(metal_arr == metallicity)[0]
+
+    model_idx = int(reduce(np.intersect1d, (age_idx, tau_idx, tauv_idx, metal_idx)))
+
+    model_flam = model_comp_spec_withlines_mmap[model_idx]
+
+    chosen_age = 10**log_age_arr[model_idx] / 1e9
+    chosen_tau = tau_gyr_arr[model_idx]
+    chosen_av = 1.086 * tauv_arr[model_idx]
+    chosen_metallicity = metal_arr[model_idx]
+
+    print "Chosen model index:", model_idx
+    print "Chosen model parameters -- "
+    print "Age [Gyr]:", chosen_age
+    print "Tau [Gyr]:", chosen_tau
+    print "A_v:", chosen_av
+    print "Metallicity [abs. frac.]:", chosen_metallicity
+
+    # Plot to check that you have the correct template
+    if plotcheck:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        ax.plot(model_lam_grid_withlines_mmap, model_flam)
+
+        # Limtis and scales
+        ax.set_xscale('log')
+        ax.set_xlim(500, 100000)
+
+        # Text on plot
+        ax.text(0.03, 0.95, r'$\mathrm{Age\,[Gyr]\,=\,}$' + str("{:.2f}".format(chosen_age)), \
+            verticalalignment='top', horizontalalignment='left', \
+            transform=ax.transAxes, color='k', size=12)
+        ax.text(0.03, 0.9, r'$\tau\,\mathrm{[Gyr]}\,=\,$' + str("{:.2f}".format(chosen_tau)), \
+            verticalalignment='top', horizontalalignment='left', \
+            transform=ax.transAxes, color='k', size=12)
+        ax.text(0.03, 0.85, r'$A_V\,=\,$' + str("{:.1f}".format(chosen_av)), \
+            verticalalignment='top', horizontalalignment='left', \
+            transform=ax.transAxes, color='k', size=12)
+        ax.text(0.03, 0.8, r'${\rm Z}\,=\,$' + str("{:.2f}".format(chosen_metallicity)), \
+            verticalalignment='top', horizontalalignment='left', \
+            transform=ax.transAxes, color='k', size=12)
+
+        plt.show()
+
+    return model_flam
 
 def plot_for_pclusters():
 
@@ -382,7 +501,7 @@ def plot_for_pclusters():
     for count in range(len(all_templates)):
         template = all_templates[count]
         template_name = all_template_names[count]
-        do_all_mods_plot(template, template_name, all_filters, filter_names, phot_lam, g141)
+        do_all_mods_plot(template, template_name, all_filters, filter_names, phot_lam, g141, 'G141')
 
     return None
 
@@ -396,7 +515,7 @@ def plot_for_g165():
     f814w = np.genfromtxt(massive_galaxies_dir + 'grismz_pipeline/for_hst_cluster_proposal/F814W_ACS.txt', \
         dtype=None, names=['wav', 'trans'])
     f105w = np.genfromtxt(massive_galaxies_dir + 'grismz_pipeline/wfc3_ir_filters/F105W_IR_throughput.csv', \
-        dtype=None, names=['wav', 'trans'], usecols=(1,2), delimiter=',')
+        dtype=None, names=['wav', 'trans'], usecols=(1,2), delimiter=',', skip_header=1)
 
     # Read G102 throughput curve and save to ascii file
     # This has to come from pysynphot # It should not need any scaling # Check with handbook
@@ -429,6 +548,9 @@ def plot_for_g165():
     irac3['wav'] *= 1e4
     irac4['wav'] *= 1e4
 
+    all_filters = [f435w, f606w, f814w, f105w, irac1, irac2, irac3, irac4]
+    filter_names = ['F435W', 'F606W', 'F814W', 'F105W', 'IRAC_CH1', 'IRAC_CH2', 'IRAC_CH3', 'IRAC_CH4']
+
     # Multiply by scale factor to make the curves look like those in the handbook
     # See the comments in the plot_for_pclusters() function above
     f435w_scalefac = 0.37  / max(f435w['trans'])
@@ -459,18 +581,74 @@ def plot_for_g165():
     # ---------------------------------- Now get the BC03 templates ---------------------------------- #
     """
     1. Elliptical galaxy with modest H-alpha emission at z=0.35
-    I'm choosing a solar metallicity template with a 4 Gyr old population with no dust and a tau=2 Gyr.
+    I'm choosing a solar metallicity template with a 6 Gyr old population with no dust and a tau=1 Gyr.
     2. Starforming galaxy at z=2
-    I'm choosing a solar metallicity template with a 50 Myr old population with no dust and a tau=0.5 Gyr.
+    I'm choosing a solar metallicity template with a 50 Myr old population with no dust and a tau=0.2 Gyr.
+    """
+    
+    # -------------------
+    # For the purposes of this code the following block to save the templates only needs to be run once.
+    # -------------------
+    """
+    # Generate lists including parameter values for as many templates as needed
+    template_metallicities = [0.02, 0.02]
+    # Since my CSPs only have solar metallicity and I'm including a tau
+    # the chosen SED will have solar metallicity by default.
+    template_ages_gyr = [6.0, 0.05]  # in Gyr
+    template_tau_gyr = [1.0, 0.2]  # in Gyr
+    template_tauv = [0.0, 0.0]  # make sure this is a multiple of 0.2 because that is the grid spacing
+    redshifts = [0.35, 2.0]
+    template_names = ['E_6gyr_z0p35', 'SF_50Myr_z2']
+
+    # Read in all models and parameters
+    model_lam_grid_withlines_mmap = np.load(figs_dir + 'model_lam_grid_withlines.npy', mmap_mode='r')
+    model_comp_spec_withlines_mmap = np.load(figs_dir + 'model_comp_spec_withlines.npy', mmap_mode='r')
+
+    log_age_arr = np.load(figs_dir + 'log_age_arr.npy', mmap_mode='r')
+    metal_arr = np.load(figs_dir + 'metal_arr.npy', mmap_mode='r')
+    tau_gyr_arr = np.load(figs_dir + 'tau_gyr_arr.npy', mmap_mode='r')
+    tauv_arr = np.load(figs_dir + 'tauv_arr.npy', mmap_mode='r')
+
+    for i in range(len(template_ages_gyr)):
+        current_age = np.log10(template_ages_gyr[i] * 1e9)  # because the saved age parameter is the log(age[yr])
+        current_tau = template_tau_gyr[i] # because the saved tau is in Gyr
+        current_tauv = template_tauv[i]
+        current_metallicity = template_metallicities[i]
+        z = redshifts[i]
+
+        template_flam = get_template(current_age, current_tau, current_tauv, current_metallicity, \
+            log_age_arr, metal_arr, tau_gyr_arr, tauv_arr, \
+            model_lam_grid_withlines_mmap, model_comp_spec_withlines_mmap, plotcheck=True)
+
+        # Redshift template and save ascii files to conform with the remaining code structure
+        template_wav = model_lam_grid_withlines_mmap * (1+z)
+        template_flux = template_flam / (1+z)
+
+        # Normalize just liek the previous ones
+        idx5500 = np.argmin(abs(model_lam_grid_withlines_mmap - 5500.0)) 
+        template_flux_at5500 = template_flux[idx5500]
+        template_flux /= template_flux_at5500
+
+        # Now save
+        template_dat = np.array(zip(template_wav, template_flux), dtype=[('template_wav', np.float64), ('template_flux', np.float64)])
+        np.savetxt(massive_galaxies_dir + 'grismz_pipeline/for_hst_cluster_proposal/' + template_names[i] + '.txt', \
+            template_dat, fmt=['%.2e', '%.4e'], header='wav flam_norm')
     """
 
-    template_metallicity = [0.02]
-    template_ages_gyr = [4.0, 0.05]
-    template_tau_gyr = [2, 0.5]
+    # ---- Now read the templates back in from the ascii files
+    ell6gyr = np.genfromtxt(massive_galaxies_dir + 'grismz_pipeline/for_hst_cluster_proposal/E_6gyr_z0p35.txt', \
+        dtype=None, names=True) 
+    sf50myr = np.genfromtxt(massive_galaxies_dir + 'grismz_pipeline/for_hst_cluster_proposal/SF_50Myr_z2.txt', \
+        dtype=None, names=True)
 
-    # Read in all models
-    model_lam_grid_withlines_mmap = np.load(figs_data_dir + 'model_lam_grid_withlines.npy', mmap_mode='r')
-    model_comp_spec_withlines_mmap = np.load(figs_data_dir + 'model_comp_spec_withlines.npy', mmap_mode='r')
+    all_templates = [ell6gyr, sf50myr]
+    all_template_names = ['Ell 6Gyr z=0.35', 'SF 50Myr z=2']
+
+    # ---------------------------------- Convolve templates with filters ---------------------------------- #
+    for count in range(len(all_templates)):
+        template = all_templates[count]
+        template_name = all_template_names[count]
+        do_all_mods_plot(template, template_name, all_filters, filter_names, phot_lam, g102, 'G102')
 
     return None
 
