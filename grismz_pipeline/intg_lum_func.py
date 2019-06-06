39203a77707f5ac7e73deb5f07c2907d2570ac5a
Bhavin Joshi <bajoshi@asu.edu>
2019-06-06 15:15:32 -0500

    Started code to properly account for luminosity func in number density estimation

---
+++ grismz_pipeline/intg_lum_func.py
@@ -0,0 +1,32 @@
+from __future__ import division
+
+import numpy as np
+from astropy.modeling import models, fitting
+from astropy.convolution import Gaussian1DKernel
+from scipy.integrate import simps
+
+import os
+import sys
+
+def integrate_lum_func():
+
+    return ngal
+
+def get_comoving_volume(redshift):
+    """
+    Given the redshift, this function 
+    computes the proper distance to the 
+    redshift and the comoving volume
+    within the slice up to that redshift.
+    """
+    
+
+    return comov_vol
+
+def main():
+
+    return None
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
