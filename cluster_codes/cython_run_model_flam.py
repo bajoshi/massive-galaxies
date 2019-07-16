b197fc7a8b90754c95f8b4aea5bc7e22a05feaf4
Bhavin Joshi <bajoshi@asu.edu>
2019-07-16 13:43:36 -0500

    New structure for cython version of model flam

---
+++ cluster_codes/cython_run_model_flam.py
@@ -0,0 +1,103 @@
+from __future__ import division
+
+import numpy as np
+
+import cython_save_model_flam as cym
+
+def main():
+
+    # Start time
+    start = time.time()
+    dt = datetime.datetime
+    print "Starting at --", dt.now()
+
+    # Read in models with emission lines adn put in numpy array
+    total_models = 37761
+
+    model_lam_grid_withlines_mmap = np.load(figs_data_dir + 'model_lam_grid_withlines.npy', mmap_mode='r')
+    model_comp_spec_withlines_mmap = np.load(figs_data_dir + 'model_comp_spec_withlines.npy', mmap_mode='r')
+
+    # total run time up to now
+    print "All models now in numpy array and have emission lines. Total time taken up to now --", time.time() - start, "seconds."
+
+    # ------------------------------- Read in filter curves ------------------------------- #
+    """
+    The HST filters, in their PYSYNPHOT form, cannot be used in conjunction with
+    the joblib module. So this function will read them and save them into numpy arrays.
+    This function has to be run once to convert the HST filters to text files that 
+    can be read with genfromtxt.
+    """
+    #save_hst_filters_to_npy()
+
+    filter_curve_dir = figs_data_dir + 'massive-galaxies/grismz_pipeline/'
+
+    uband_curve = np.genfromtxt(filter_curve_dir + 'kpno_mosaic_u.txt', dtype=None, \
+        names=['wav', 'trans'], skip_header=14)
+    f435w_filt_curve = np.genfromtxt(filter_curve_dir + 'f435w_filt_curve.txt', \
+        dtype=None, names=['wav', 'trans'])
+    f606w_filt_curve = np.genfromtxt(filter_curve_dir + 'f606w_filt_curve.txt', \
+        dtype=None, names=['wav', 'trans'])
+    f775w_filt_curve = np.genfromtxt(filter_curve_dir + 'f775w_filt_curve.txt', \
+        dtype=None, names=['wav', 'trans'])
+    f850lp_filt_curve = np.genfromtxt(filter_curve_dir + 'f850lp_filt_curve.txt', \
+        dtype=None, names=['wav', 'trans'])
+    f125w_filt_curve = np.genfromtxt(filter_curve_dir + 'f125w_filt_curve.txt', \
+        dtype=None, names=['wav', 'trans'])
+    f140w_filt_curve = np.genfromtxt(filter_curve_dir + 'f140w_filt_curve.txt', \
+        dtype=None, names=['wav', 'trans'])
+    f160w_filt_curve = np.genfromtxt(filter_curve_dir + 'f160w_filt_curve.txt', \
+        dtype=None, names=['wav', 'trans'])
+    irac1_curve = np.genfromtxt(filter_curve_dir + 'irac1.txt', dtype=None, \
+        names=['wav', 'trans'], skip_header=3)
+    irac2_curve = np.genfromtxt(filter_curve_dir + 'irac2.txt', dtype=None, \
+        names=['wav', 'trans'], skip_header=3)
+    irac3_curve = np.genfromtxt(filter_curve_dir + 'irac3.txt', dtype=None, \
+        names=['wav', 'trans'], skip_header=3)
+    irac4_curve = np.genfromtxt(filter_curve_dir + 'irac4.txt', dtype=None, \
+        names=['wav', 'trans'], skip_header=3)
+
+    # IRAC wavelengths are in mixrons # convert to angstroms
+    irac1_curve['wav'] *= 1e4
+    irac2_curve['wav'] *= 1e4
+    irac3_curve['wav'] *= 1e4
+    irac4_curve['wav'] *= 1e4
+
+    all_filters = [uband_curve, f435w_filt_curve, f606w_filt_curve, f775w_filt_curve, f850lp_filt_curve, \
+    f125w_filt_curve, f140w_filt_curve, f160w_filt_curve, irac1_curve, irac2_curve, irac3_curve, irac4_curve]
+    all_filter_names = ['u', 'f435w', 'f606w', 'f775w', 'f850lp', \
+    'f125w', 'f140w', 'f160w', 'irac1', 'irac2', 'irac3', 'irac4']
+
+    # Loop over all redshifts and filters and compute magnitudes
+    zrange = np.arange(0.005, 6.005, 0.005)
+    print "Redshift grid for models:"
+    print zrange
+
+    max_cores = 2
+
+    for i in range(int(np.ceil(len(all_filters)/max_cores))):
+
+        jmin = i*max_cores
+        jmax = (i+1)*max_cores
+
+        if jmax > len(all_filters):
+            jmax = len(all_filters)
+
+        # Will use as many cores as filters
+        processes = [mp.Process(target=cym.compute_filter_flam, args=(all_filters[j]['wav'], all_filters[j]['trans'], \
+            all_filter_names[j], start, \
+            model_comp_spec_withlines_mmap, model_lam_grid_withlines_mmap, total_models, zrange)) \
+            for j in range(len(all_filters[jmin:jmax]))]
+        for p in processes:
+            p.start()
+            print "Current process ID:", p.pid
+        for p in processes:
+            p.join()
+
+        print "Finished with filters:", all_filter_names[jmin:jmax]
+
+    print "All done. Total time taken:", time.time() - start
+    return None
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
