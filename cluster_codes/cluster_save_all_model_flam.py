ed2054dcd21f29029f69fd996199d7579ea9de57
Bhavin Joshi <bajoshi@asu.edu>
2019-07-17 18:27:25 -0500

    Restarting on agave because it quit before finishing

--- cluster_codes/cluster_save_all_model_flam.py
+++ cluster_codes/cluster_save_all_model_flam.py
@@ -126,10 +126,14 @@ def main():
     irac3_curve['wav'] *= 1e4
     irac4_curve['wav'] *= 1e4
 
-    all_filters = [uband_curve, f435w_filt_curve, f606w_filt_curve, f775w_filt_curve, f850lp_filt_curve, \
-    f125w_filt_curve, f140w_filt_curve, f160w_filt_curve, irac1_curve, irac2_curve, irac3_curve, irac4_curve]
-    all_filter_names = ['u', 'f435w', 'f606w', 'f775w', 'f850lp', \
-    'f125w', 'f140w', 'f160w', 'irac1', 'irac2', 'irac3', 'irac4']
+    #all_filters = [uband_curve, f435w_filt_curve, f606w_filt_curve, f775w_filt_curve, f850lp_filt_curve, \
+    #f125w_filt_curve, f140w_filt_curve, f160w_filt_curve, irac1_curve, irac2_curve, irac3_curve, irac4_curve]
+    #all_filter_names = ['u', 'f435w', 'f606w', 'f775w', 'f850lp', \
+    #'f125w', 'f140w', 'f160w', 'irac1', 'irac2', 'irac3', 'irac4']
+
+    all_filters = [uband_curve, f435w_filt_curve, f775w_filt_curve, \
+    f140w_filt_curve, irac3_curve, irac4_curve]
+    all_filter_names = ['u', 'f435w', 'f775w', 'f140w', 'irac3', 'irac4']
 
     # Loop over all redshifts and filters and compute magnitudes
     zrange = np.arange(0.005, 6.005, 0.005)
